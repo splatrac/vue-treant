5974e12b7b9a5e944bf56a9086264618ece36c6c
craftrac <dev@orbitplum.com>
2024-10-10 15:43:55 +0300

    changes in events

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,79 +1,87 @@
 <template>
-  <ul id="treemain" class="ptree">
-    <template v-for="node in treeNodes" :key="node.title">
-        <li class="container">
-            <TreeNode :hasChildren="Object.hasOwn(node, 'children')" @expand="expandNode(node)"> 
-                <template #title>
-                    {{ node.title }}
-                </template>
-            </TreeNode>
-            <Tree v-if="node.children"
-              v-show="node.isExpanded"
-              :data="node.children"
-            />
-        </li>
-    </template>
-  </ul>
+    <ul id="treemain" class="ptree">
+        <template v-for="node in treeNodes" :key="node.title">
+            <li class="container">
+                <TreeNode :hasChildren="Object.hasOwn(node, 'children')" @expanded="expandNode(node)"
+                    @activated="activateNode(node)">
+                    <template #title>
+                        {{ node.title }}
+                    </template>
+                </TreeNode>
+                <Tree v-if="node.children" v-show="node.isExpanded" :data="node.children" />
+            </li>
+        </template>
+    </ul>
 </template>
 
 <script setup lang="ts">
-    import { reactive } from 'vue'
-    import TreeNode from './Node.vue'
-    import { Node } from '../types'
+import { reactive } from 'vue'
+import TreeNode from './Node.vue'
+import { Node } from '../types'
 
-    const props = defineProps<{ data: Node[] }>()
-    const treeNodes = reactive(props.data)
-    const expandNode = (node: Node) => {
-        node.isExpanded = !node.isExpanded
-    }
+const props = defineProps<{ data: Node[] }>()
+const treeNodes = reactive(props.data)
+const expandNode = (node: Node) => {
+    node.isExpanded = !node.isExpanded
+}
+const activateNode = (node: Node) => {
+    node.isActive = !node.isActive
+}
 </script>
 <style scoped>
-
 .ptree li {
-  list-style-type: none;
+    list-style-type: none;
 }
 
 .ptree .menuIcon {
-  /* margin-left: auto; */
-  color: black;
+    /* margin-left: auto; */
+    color: black;
 }
 
 .ptree .drag-handler {
-  color: #ef5350;
+    color: #ef5350;
 }
 
 .ptree .drag-handler i {
-  pointer-events: none;
+    pointer-events: none;
 }
 
 .ptree {
-  height: 100%;
+    height: 100%;
 }
 
-.disabled > * {
-  pointer-events: none;
+.disabled>* {
+    pointer-events: none;
 }
 
 .ptreemenuCont i {
-  pointer-events: none;
+    pointer-events: none;
 }
 
 .ptree ul.not-active {
-  max-height: 0;
-  transition: max-height 0.25s ease-out;
-  overflow: hidden;
+    max-height: 0;
+    transition: max-height 0.25s ease-out;
+    overflow: hidden;
 }
 
-.dragging-element li { 
+.dragging-element li {
     border: 0px !important;
 }
 
-.ptree::-webkit-scrollbar { width: 2px !important;}
-.ptree::-webkit-scrollbar-track { background: whitesmoke !important;}
-.ptree::-webkit-scrollbar-thumb { background: grey !important; }
+.ptree::-webkit-scrollbar {
+    width: 2px !important;
+}
+
+.ptree::-webkit-scrollbar-track {
+    background: whitesmoke !important;
+}
+
+.ptree::-webkit-scrollbar-thumb {
+    background: grey !important;
+}
 
 /* fixes menu click event problem */
-.menuIcon > svg {
-  pointer-events: none;
+.menuIcon>svg {
+    pointer-events: none;
 }
 </style>
